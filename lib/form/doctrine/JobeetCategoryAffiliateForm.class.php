ec087dfc99ae0ff1c1465891c0020cb7a82e1a3c
moriame <moriame@moriame-laptop.(none)>
2010-12-26 18:11:21 +0100

    Initial commit of all files to the repository

---
+++ lib/form/doctrine/JobeetCategoryAffiliateForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * JobeetCategoryAffiliate form.
+ *
+ * @package    jobeet
+ * @subpackage form
+ * @author     loïc moriamé
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class JobeetCategoryAffiliateForm extends BaseJobeetCategoryAffiliateForm
+{
+  public function configure()
+  {
+  }
+}
